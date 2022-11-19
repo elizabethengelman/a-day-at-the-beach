19312205bf3e32bf97f36c002f0e498cbb9b0f5b
Elizabeth Engelman <elizabeth@redsquirrel.com>
2022-11-19 16:56:39 -0500

    Migrate sketch from react to this project

--- public/sketch.js
+++ public/sketch.js
@@ -1,16 +1,12 @@
-// Copyright (c) 2020 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-/* ===
-ml5 Example
-BodyPix
-=== */
+const width = 400;
+const height = 300;
 
 let bodypix;
 let video;
 let segmentation;
+let sunscreenBuff;
+let sunscreenClone;
+
 const options = {
   outputStride: 8, // 8, 16, or 32, default is 16
   segmentationThreshold: 0.3, // 0 - 1, defaults to 0.5
@@ -21,23 +17,16 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(320, 240);
-  // load up your video
+  createCanvas(width, height);
   video = createCapture(VIDEO, videoReady);
   video.size(width, height);
+  video.hide();
+
+  sunscreenBuff = createGraphics(width, height);
 }
 
 function videoReady() {
   bodypix.segment(video, gotResults);
-}
-
-function draw() {
-  background(0);
-  if (segmentation) {
-    console.log(segmentation);
-    image(segmentation.backgroundMask, 0, 0, width, height);
-  }
-  // eslint-disable-next-line no-plusplus
 }
 
 function gotResults(error, result) {
@@ -47,4 +36,21 @@ function gotResults(error, result) {
   }
   segmentation = result;
   bodypix.segment(video, gotResults);
+}
+
+function draw() {
+  image(video, 0, 0);
+
+  sunscreenBuff.noStroke();
+  sunscreenBuff.fill(255, 0, 0);
+  sunscreenBuff.ellipse(mouseX, mouseY, 33, 33);
+
+  sunscreenClone = sunscreenBuff.get();
+
+  if (segmentation) {
+    sunscreenClone.mask(segmentation.backgroundMask);
+  }
+
+  tint(255, 40);
+  image(sunscreenClone, 0, 0);
 }
