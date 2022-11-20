4a254342e64503df42a78f83bec74db3fb6a182d
Elizabeth Engelman <elizabeth@redsquirrel.com>
2022-11-20 17:35:33 -0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        beach: "url(/images/beach.gif)",
+      },
+      animation: {
+        brighten: "brightend .5s linear",
+      },
+      keyframes: {
+        brighten: {
+          "0%": { opacity: "80" },
+          "100%": { opacity: "100" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
